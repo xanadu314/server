29a3f29e67403c8b3100d99393cc06fd50f0e3e1
xanadu314 <37304315+xanadu314@users.noreply.github.com>
2019-05-14 18:18:05 +0800

    summary

---
+++ hpc.ncpgr.cn_8093/mediawiki/apidf90.php
@@ -0,0 +1,11 @@
+<?xml version="1.0"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="zh-cn">
+		<id>http://hpc.ncpgr.cn:8093/mediawiki/api.php?hidebots=1&amp;days=7&amp;limit=50&amp;action=feedrecentchanges&amp;feedformat=atom</id>
+		<title>作物遗传改良国家重点实验室集群系统  - 最近更改 [zh-cn]</title>
+		<link rel="self" type="application/atom+xml" href="http://hpc.ncpgr.cn:8093/mediawiki/api.php?hidebots=1&amp;days=7&amp;limit=50&amp;action=feedrecentchanges&amp;feedformat=atom"/>
+		<link rel="alternate" type="text/html" href="http://hpc.ncpgr.cn:8093/mediawiki/index.php/%E7%89%B9%E6%AE%8A:%E6%9C%80%E8%BF%91%E6%9B%B4%E6%94%B9"/>
+		<updated>2019-05-13T09:16:34Z</updated>
+		<subtitle>用这个源跟踪本wiki的最近更改。</subtitle>
+		<generator>MediaWiki 1.24.1</generator>
+
+	</feed>
